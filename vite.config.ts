cb86a2996c91d1d581a880459e5d2189c03ec744
Nicolet <jmlee337@gmail.com>
2026-03-07 10:55:48 -0600

    admin rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin/index.html"),
+      },
+    },
+  },
   plugins: [
     react({
       babel: {
